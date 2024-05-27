9f2c074b4cfdd4b607be9f506649afc8c9e9ab1a
Leonel Menci <minterger@gmail.com>
2024-05-26 23:03:10 -0300

    update model Empresa

--- src/models/Empresa.js
+++ src/models/Empresa.js
@@ -4,6 +4,9 @@ const ObjectId = Schema.ObjectId;
 
 const EmpresaSchema = new Schema({
   empresa: { type: String, required: true },
+  //type puede ser transporte, cliente o ambos
+  type: { type: String, required: true },
+  //id tributaria puede ser DNI o RUT o lo que se use en ese pais
   id_tributaria: { type: String, required: true },
   vehiculo: [
     {
@@ -15,6 +18,12 @@ const EmpresaSchema = new Schema({
     {
       type: ObjectId,
       ref: "Chofer",
+    },
+  ],
+  viajes: [
+    {
+      type: ObjectId,
+      ref: "Viaje",
     },
   ],
 });
