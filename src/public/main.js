f43665fb1d4a34e3a4c5950440933f9777b4f2d4
Leonel Menci <minterger@gmail.com>
2024-08-09 22:41:36 -0300

    feat: Add form sections for traffic data and contracted company
    The code changes include adding form sections for traffic data and contracted company in the HTML file. This allows users to input information related to traffic and the company they are contracting.

---
+++ src/public/main.js
@@ -0,0 +1,32 @@
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+const desplegarSection = (id) => {
+  const section = document.querySelector(id);
+  section.classList.toggle("hidden");
+  section.classList.toggle("flex");
+};
+
+const button_trafico = document.querySelector("#button_trafico");
+const button_empresa = document.querySelector("#button_empresa");
+
+button_trafico.addEventListener("click", () => {
+  desplegarSection("#trafico");
+});
+
+button_empresa.addEventListener("click", () => {
+  desplegarSection("#empresa");
+});
+
+// obtener datos de los inputs
+const inputs = document.querySelectorAll("input");
+const datos = {};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (event) => {
+    datos[event.target.name] = event.target.value;
+  });
+});
