f542b240eaf9d667f2883cfd8db8966415c72819
Minterger <minterger@gmail.com>
2024-09-20 11:59:38 -0300

    Actualizar etiqueta de descripción y formato de datos en el formulario de contrato de flete

--- src/public/main.js
+++ src/public/main.js
@@ -15,43 +15,96 @@ form.addEventListener("submit", (event) => {
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
-      a.download = `Contrato de Flete - ${datos.chofer} - ${
+      a.download = `Contrato de Flete - ${datos.chofer
+        ?.trim()
+        .toUpperCase()} - ${
         datos.crt || datos.remito || "N/A"
-      } - ${datos.razon_social}.pdf`;
+      } - ${datos.razon_social?.trim().toUpperCase()}.pdf`;
       a.click();
       URL.revokeObjectURL(url);
     });
 });
 
-const desplegarSection = (id) => {
+const desplegarSection = (id, e) => {
   const section = document.querySelector(id);
   ["visible", "invisible", "opacity-0", "max-h-screen", "max-h-0"].map(
     (className) => section.classList.toggle(className)
   );
+
+  // tagName is a h2
+  if (e.target.tagName === "H2") {
+    e.target.querySelector("box-icon").classList.toggle("rotate-90");
+    return;
+  } else {
+    e.target.classList.toggle("rotate-90");
+  }
 };
 
-button_trafico.addEventListener("click", () => {
-  desplegarSection("#trafico");
+button_trafico.addEventListener("click", (e) => {
+  desplegarSection("#trafico", e);
 });
 
-button_empresa.addEventListener("click", () => {
-  desplegarSection("#empresa");
+button_empresa.addEventListener("click", (e) => {
+  desplegarSection("#empresa", e);
 });
 
-button_unidad.addEventListener("click", () => {
-  desplegarSection("#unidad");
+button_unidad.addEventListener("click", (e) => {
+  desplegarSection("#unidad", e);
 });
 
-button_contratacion.addEventListener("click", () => {
-  desplegarSection("#contratacion");
+button_contratacion.addEventListener("click", (e) => {
+  desplegarSection("#contratacion", e);
 });
 
-button_facturacion.addEventListener("click", () => {
-  desplegarSection("#facturacion");
+button_facturacion.addEventListener("click", (e) => {
+  desplegarSection("#facturacion", e);
 });
 
-button_descripcion.addEventListener("click", () => {
-  desplegarSection("#descripcion");
+button_descripcion.addEventListener("click", (e) => {
+  desplegarSection("#descripcion", e);
+});
+
+// desplegar todos los sections
+const button_desplegar = document.querySelector("#desplegar");
+
+// desplegar todos los sections y ver si estan desplegados o no antes de hacer click
+button_desplegar.addEventListener("click", () => {
+  // data-desplegado="false"
+
+  if (button_desplegar.dataset.desplegado === "false") {
+    const divsInvisible = document.querySelectorAll("div.invisible");
+    divsInvisible.forEach((div) => {
+      const h2 = div.previousElementSibling;
+      h2.querySelector("box-icon").classList.toggle("rotate-90");
+      ["invisible", "max-h-0", "opacity-0"].map((className) =>
+        div.classList.remove(className)
+      );
+      ["visible", "max-h-screen"].map((className) =>
+        div.classList.add(className)
+      );
+
+      button_desplegar.dataset.desplegado = "true";
+    });
+  } else {
+    const divsVisible = document.querySelectorAll("div.visible");
+    divsVisible.forEach((div) => {
+      const h2 = div.previousElementSibling;
+      h2.querySelector("box-icon").classList.toggle("rotate-90");
+      ["visible", "max-h-screen"].map((className) =>
+        div.classList.remove(className)
+      );
+      ["opacity-0", "max-h-0", "invisible"].map((className) =>
+        div.classList.add(className)
+      );
+
+      button_desplegar.dataset.desplegado = "false";
+    });
+  }
+
+  button_desplegar.textContent =
+    button_desplegar.dataset.desplegado === "true"
+      ? "Colapsar todos los campos"
+      : "Desplegar todos los campos";
 });
 
 // obtener datos de los inputs
